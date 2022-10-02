86443534ec21968a9133bac810c8e408f70e2d97
Fecso <Fecsoul@gmail.com>
2022-10-01 22:43:53 -0300

    Adhesion Forms release v3

--- router/form.js
+++ router/form.js
@@ -2,7 +2,7 @@ const {Router} = require ('express');
 const { crearForm, recuperarForm, actualizarForm, eliminarForm } = require('../controllers/getForm');
 
 const router = Router();
-router.post('/createForm', crearForm)
+router.post('/crearForm', crearForm)
 router.post('/getForm', recuperarForm)
 router.post('/actualizarForm', actualizarForm)
 router.post('/eliminarForm', eliminarForm)
