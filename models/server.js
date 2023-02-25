5245c3b4fb3ddd29d7c0ac8772bfaf1e22eb87a6
Francisco Cantero <fcantero@finnegans.com.ar>
2023-02-25 16:28:29 -0300

    Cors

--- models/server.js
+++ models/server.js
@@ -27,7 +27,7 @@ class Server {
     middlewares() {
         // Desplegar el directorio público
         this.app.use( express.static( path.resolve( __dirname, '../public' ) ) );
-       
+        this.app.options('*', cors())
         // CORS
         this.app.use(cors())
         //Parseo del body 
