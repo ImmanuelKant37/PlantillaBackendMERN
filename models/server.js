694c5d97f1e017318a2ab2787cf5dcd4b169fb3f
Francisco Cantero <fcantero@finnegans.com.ar>
2023-02-25 18:11:42 -0300

    Cors post fix4

--- models/server.js
+++ models/server.js
@@ -30,7 +30,7 @@ class Server {
         var whitelist = ['https://gestorproyectos.netlify.app', 'http://localhost:3000/']
         var corsOptionsDelegate = function (req, callback) {
           var corsOptions;
-          if (whitelist.indexOf(req.header('Origin')) !== -1) {
+          if (whitelist.indexOf(req.header('Referer')) !== -1) {
             corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
           } else {
             corsOptions = { origin: false } // disable CORS for this request
